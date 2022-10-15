8e6041a7a9bbe2497b6347e0fb0298200d5bb4ae
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-10-15 14:01:15 +0600

    start today

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const SparkLine = () => {
-  return <div>SparkLine</div>;
+const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      dataSource={data}
+      lineWidth={1}
+      fill={color}
+      valueType="Numeric"
+      border={{ color: currentColor, width: 2 }}
+      xName="x"
+      yName="yval"
+      type={type}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 };
 
 export default SparkLine;
