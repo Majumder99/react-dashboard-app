c673441ab6de2e8fbb4def671eee4e4d4de3681b
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-10-16 05:00:21 +0600

    added app

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,6 +13,20 @@ const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClick, setIsClick] = useState(initialState);
   const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState("#03C9D7");
+  const [currentMode, setCurrentMode] = useState("Light");
+  const [themeSettings, setThemeSettings] = useState(false);
+
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+    localStorage.setItem("themeMode", e.target.value);
+  };
+
+  const setColor = (e) => {
+    setCurrentColor(e.target.value);
+    localStorage.setItem("themeColor", e.target.value);
+  };
+
   const handleClick = (clicked) => {
     setIsClick({ ...initialState, [clicked]: true });
   };
@@ -26,6 +40,12 @@ const ContextProvider = ({ children }) => {
         handleClick,
         screenSize,
         setScreenSize,
+        currentColor,
+        currentMode,
+        setCurrentColor,
+        setCurrentMode,
+        themeSettings,
+        setThemeSettings,
       }}
     >
       {/* always have to return the children */}
